d94c409147b4e356a27658ae663467a89b92733c
Jaeyeong Cheon <chun3720@hotmail.com>
2022-12-12 20:24:11 +0900

    minor

--- app.py
+++ app.py
@@ -2,9 +2,10 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 
+file = st.file_uploader(label="select file ")
 
 def predict():
-    file = st.file_uploader(label="select file ")
+    
     df = pd.read_csv(file, delimiter="\s+")
     st.success(f"{df.columns}")
 
